be2c45cb0fe6d7107ad9a6f15f0e32245bf77ff8
PC-Ow <PC-Ow@PC-OWNER>
2024-05-09 03:57:05 +0900

    プロジェクト ファイルを追加します。

---
+++ 2024_05_09_Task7/2024_05_09_Task7.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int Bit(int x) {
+    return 1 << x;
+}
+
+void Show(char flag) {
+    for (int i = 0; i < 8; i++) {
+        printf("%d番目%s\n", i + 1 , (flag & Bit(i)) ? "True" : "False");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    unsigned char flag = 0;
+
+    Show(flag);
+
+    int select = 0;
+    printf("何番目のフラグを立てますか\n");
+    cin >> select;
+
+    if (1 <= select && select <= 8) {
+        flag |= Bit(select - 1);
+    }
+    
+    Show(flag);
+
+
+    return 0;
+}
